13e55c16ed1dfc0fc49b20657ffdb53c6c297330
Sooraj <s.sooraj1@ibm.com>
2026-06-15 12:11:56 -0400

    config updates

--- playwright.config.js
+++ playwright.config.js
@@ -38,10 +38,10 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
     headless: false,
+    screenshot: 'on',
+    video: 'off',
   },
 
   /* Configure projects for major browsers */
