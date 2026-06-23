a02de61deb33122cac95acd64a34c09daa6dea12
Sooraj <s.sooraj1@ibm.com>
2026-06-23 19:05:15 +0530

    shop aga flows start

---
+++ tests/Shop/AGA_flows.spec.js
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+import { createContextWithExtension, getPage } from './helpers/browser-context.js';
+import testData from '././testdata/aga-flows-data.json';
+
+test('AGA Device', async ({ }, testInfo) => {
+    const { browser, context } = await createContextWithExtension(testInfo.workerIndex);
+    const page = await getPage(context);
+    const data = testData['AGA_Device'];
+    try {
+        // await page.goto('https://fesa-www.ids.int.bell.ca');
+        await page.goto(data.url);
+        await page.pause();
+
+    }
+    finally {
+        await browser.close();
+    }
+});
+
+test('AGA esim', async ({ }, testInfo) => {
+    const { browser, context } = await createContextWithExtension(testInfo.workerIndex);
+    const page = await getPage(context);
+    try {
+
+    }
+    finally {
+        await browser.close();
+    }
+});
+
+test('AGA psim', async ({ }, testInfo) => {
+    const { browser, context } = await createContextWithExtension(testInfo.workerIndex);
+    const page = await getPage(context);
+    try {
+
+    }
+    finally {
+        await browser.close();
+    }
+});
